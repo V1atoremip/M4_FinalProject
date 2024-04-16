6f80ff58fa3171d131a91ad7ee4a33d60dd010b3
Парада Сергей Сергеевич <sergejparada95@gmail.com>
2024-04-16 18:41:22 +0300

    NewRunVersionProject
    -Простое управления персонажа
    -Выставления камеры

---
+++ ProjectXCommand/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speedMove = 5;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * speedMove;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * speedMove;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * speedMove;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * speedMove;
+        }
+    }
+}
